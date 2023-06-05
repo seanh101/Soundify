1309fe39661dfdae9f9b71ed2f70944dadd6ea32
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-05 10:55:31 -0400

    Initial commit.  Creates skeleton for project.
    Co-authored-by: Sean Harrington <seanharrington4189@gmail.com>
    Co-authored-by: Ian Abbene <ianabbene@gmail.com>
    Co-authored-by: Marley Sellem <mzsellem@hotmail.com>
    Co-authored-by: Chase Hilsinger <chasehilsinger@gmail.com>

---
+++ main_app/views.py
@@ -0,0 +1,28 @@
+import os
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
