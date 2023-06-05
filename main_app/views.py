baaaa440f7bc636a05b8f7a437b6e2e629dbd479
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-05 11:54:16 -0400

    Adds view functions.
    Co-authored-by: Sean Harrington <seanharrington4189@gmail.com>
    Co-authored-by: Chase Hilsinger <chasehilsinger@gmail.com>

--- main_app/views.py
+++ main_app/views.py
@@ -6,12 +6,19 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Playlist, Song
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def playlist_index(reqeust):
+    pass
+
+def playlist_detail(request):
+    pass
 
 def signup(request):
     error_message = ''
@@ -26,3 +33,28 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+class PlaylistCreate(CreateView):
+    model = Playlist
+    fields = ['name', 'description', 'is_public', 'songs']
+
+    def form_valid(self, form):
+        form.instance.user = self.reqeust.user
+        return super().form_valid(form)
+    
+class PlaylistUpdate(UpdateView):
+    model = Playlist
+    fields = ['name', 'description', 'is_public']
+
+class PlaylistDelete(DeleteView):
+     model = Playlist
+     success_url = '/playlists'
+
+def create_song(request, playlist_id):
+    pass
+
+def assoc_song(reqeust, playlist_id, song_id):
+    pass
+
+def unassoc_song(request, playlist_id, song_id):
+    pass
