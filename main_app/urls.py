0b31d3de971e4a8e9ea95a059029e967a71af6f0
seanh101 <seanharrington4189@gmail.com>
2023-06-05 16:06:28 -0400

    Create, index, and detail templates
    Co-authored-by: Jon Schiavone <jonschiavoneucf@gmail.com>
    Co-authored-by: Sean Harrington <seanharrington4189@gmail.com>
    Co-authored-by: Chase Hilsinger <chasehilsinger@gmail.com>

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('playlists/', views.playlists_index, name='playlists_index'),
     path('playlists/<int:playlist_id>/', views.playlists_detail, name='playlists_detail'),
+    path('playlists/create/', views.PlaylistCreate.as_view(), name='playlist_create'),
+    path('playlists/<int:pk>/update', views.PlaylistUpdate.as_view(), name='playlists_update'),
+    
+   
     path('accounts/signup/', views.signup, name='signup'),
+    
 ]
