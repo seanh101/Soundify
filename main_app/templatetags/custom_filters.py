e5528b9bd7890ebf72e80d792b19da400e6442a3
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-08 00:06:36 -0400

    Updates custom time format to account for lengths over 60 minutes.
    Co-authored-by: Sean Harrington <seanharrington4189@gmail.com>
    Co-authored-by: Chase Hilsinger <chasehilsinger@gmail.com>

--- main_app/templatetags/custom_filters.py
+++ main_app/templatetags/custom_filters.py
@@ -5,9 +5,14 @@ register = template.Library()
 
 @register.filter(name='duration_format')
 def duration_format(duration):
-    minutes = int(duration / 1000 // 60)
-    seconds = int(duration / 1000 % 60)
-    return f"{minutes}:{seconds:02d}"
+    seconds = int(duration / 1000) % 60
+    minutes = int(duration / 1000 // 60) % 60
+    hours = int(duration / 1000 // 3600)
+
+    if hours > 0:
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    else:
+        return f"{minutes:02d}:{seconds:02d}"
 
 @register.filter(name='aggregate_duration')
 def aggregate_duration_filter(queryset):
