9dca9ccee1e5119863814926073e713e12e8ff7d
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-07 22:58:32 -0400

    Adds custom_filter to sum song duration of playlist and display in custom format.
    Co-authored-by: Sean Harrington <seanharrington4189@gmail.com>
    Co-authored-by: Chase Hilsinger <chasehilsinger@gmail.com>

--- main_app/templatetags/custom_filters.py
+++ main_app/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Sum
 
 register = template.Library()
 
@@ -7,3 +8,9 @@ def duration_format(duration):
     minutes = int(duration / 1000 // 60)
     seconds = int(duration / 1000 % 60)
     return f"{minutes}:{seconds:02d}"
+
+@register.filter(name='aggregate_duration')
+def aggregate_duration_filter(queryset):
+    total_duration = queryset.aggregate(total_duration=Sum('duration'))['total_duration']
+    return total_duration if total_duration else 0
+
